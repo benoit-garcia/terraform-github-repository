02e0e814d004374fe94e38c186e2907491170f09
Benoit Garcia <benoit.garcia@protonmail.com>
2024-04-29 09:58:55 +0200

    feat(labels): Separate default and additional labels

--- labels.tf
+++ labels.tf
@@ -1,7 +1,7 @@
 resource "github_issue_labels" "this" {
   repository = github_repository.this.name
   dynamic "label" {
-    for_each = var.labels
+    for_each = var.create_default_labels ? merge(local.default_labels, var.labels) : var.labels
     content {
       name        = label.key
       color       = label.value["color"]
--- variables.tf
+++ variables.tf
@@ -248,48 +248,17 @@ variable "branch_protections" {
   default = {}
 }
 
+variable "create_default_labels" {
+  description = "Create github default labels"
+  type        = bool
+  default     = true
+}
+
 variable "labels" {
   description = "List of issues label"
   type = map(object({
     color       = string,
     description = optional(string, null),
   }))
-  default = {
-    "bug" = {
-      color       = "d73a4a"
-      description = "Something isn't working"
-    },
-    "documentation" = {
-      color       = "0075ca"
-      description = "Improvements or additions to documentation"
-    },
-    "duplicate" = {
-      color       = "cfd3d7"
-      description = "This issue or pull request already exists"
-    },
-    "enhancement" = {
-      color       = "a2eeef"
-      description = "New feature or request"
-    },
-    "good first issue" = {
-      color       = "7057ff"
-      description = "Good for newcomers"
-    },
-    "help wanted" = {
-      color       = "008672"
-      description = "Extra attention is needed"
-    },
-    "invalid" = {
-      color       = "e4e669"
-      description = "This doesn't seem right"
-    },
-    "question" = {
-      color       = "d876e3"
-      description = "Further information is requested"
-    },
-    "wontfix" = {
-      color       = "ffffff"
-      description = "This will not be worked on"
-    },
-  }
+  default = {}
 }
